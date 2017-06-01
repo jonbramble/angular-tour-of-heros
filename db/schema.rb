a20b2eaeca46784916138d577dba91ac06b949d3
Jonathan Bramble <jonathan.bramble@ubc.ca>
2017-05-31 18:27:51 -0700

    updated table and enviroment settings for testing production

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20170531191841) do
 
-  create_table "heros", force: :cascade do |t|
+  create_table "heroes", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
